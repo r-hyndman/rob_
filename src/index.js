86b41f1f88e1c4be2c6145bf18a15f7a8b80d593
Rob Hyndman <r.hyndman@deakin.edu.au>
2020-10-30 13:57:20 +1100

    NEW: Add `some` function and tests

--- src/index.js
+++ src/index.js
@@ -74,6 +74,13 @@ function some(array, callback) {
   return false;
 };
 
+function some(array, callback) {
+  for (let i = 0; i < array.length; ++i) {
+    if (callback(array[i], i, array)) return true;
+  }
+  return false;
+};
+
 export {
   every,
   filter,
