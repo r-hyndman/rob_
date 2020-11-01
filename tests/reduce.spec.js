140e256a572e411ed67bdf97f68df019c3cb86c7
Rob Hyndman <r.hyndman@deakin.edu.au>
2020-11-02 08:37:25 +1100

    ENHANCE: Add an initial value to one of the tests

--- tests/reduce.spec.js
+++ tests/reduce.spec.js
@@ -4,19 +4,19 @@ import {reduce} from '../src';
 const testArray = [1, 2, 3, 4];
 
 describe('reduce', () => {
-  it('should return the sum of all the values', () => {
+  it('should return the sum of all the values starting with 0', () => {
     function sumOfValues(accumulator, element) {
       return accumulator + element;
     };
 
-    const nativeResult = testArray.reduce(sumOfValues);
-    const robDashResult = reduce(testArray, sumOfValues);
+    const nativeResult = testArray.reduce(sumOfValues, 0);
+    const robDashResult = reduce(testArray, sumOfValues, 0);
 
     // robDashResult should be the same value as nativeResult
     expect(robDashResult).toEqual(nativeResult);
   });
 
-  it('should return an the highest value', () => {
+  it('should return the highest value', () => {
     function highestValue(accumulator, element) {
       return accumulator || 0 > element ? accumulator : element;
     };
